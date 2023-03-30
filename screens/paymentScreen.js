6fe4acb736dd855a1e7df15069cb605bd8f50adf
davecoders0201 <davecoder0201@gmail.com>
2023-03-30 13:04:23 +0530

    perfectly formatted

--- screens/paymentScreen.js
+++ screens/paymentScreen.js
@@ -3,8 +3,6 @@ import {loadStripe} from '@stripe/stripe-js';
 import {Elements} from '@stripe/react-stripe-js';
 import CheckoutForm from './CheckoutForm';
 
-console.log('key-', process.env);
-
 // This is the public key of the stripe which is display when you create your account in the stripe
 const PUBLIC_KEY =
   'pk_test_51MOxakSBrlmuoaW8d4n4QEG3OuCl7lPniqQbLnhuRYAC4fGR9EdVDpvum8sct7TvAy2cnJBoWYLoEMDYWo6JRPfu00vCnGACp0';
