50f8d865235d49bf74032fec89ab11d2fc127cb1
davecoders0201 <davecoder0201@gmail.com>
2023-03-30 17:53:41 +0530

    Backend added

--- screens/productList.js
+++ screens/productList.js
@@ -1,123 +1,203 @@
-import {View, TouchableOpacity} from 'react-native';
-import React from 'react';
+// This file uses the Static data API to map and display
+
+// import {View, TouchableOpacity} from 'react-native';
+// import React from 'react';
+// import ProductListContain from './productListContain';
+// import {smartWatchesData} from './../data/smartWatchesData';
+// import {ScrollView} from 'react-native-gesture-handler';
+// import {tabletsData} from './../data/tabletsData';
+// import {LaptopData} from './../data/laptopData';
+// import {monitorData} from './../data/monitorData';
+
+// const ProductList = ({navigation, route}) => {
+//   // const id = route.params?.productId;
+//   // console.log('id:', id);
+//   // let selectedProduct;
+//   // if (id === 0) {
+//   //   const selectedProduct = smartWatchesData.find(element => {
+//   //     console.log(element);
+//   //     return id === element.id;
+//   //   });
+//   // } else if (id === 1) {
+//   //   const selectedProduct = tabletsData.find(element => {
+//   //     console.log(element);
+//   //     return id === element.id;
+//   //   });
+//   // } else if (id === 2) {
+//   //   const selectedProduct = LaptopData.find(element => {
+//   //     console.log(element);
+//   //     return id === element.id;
+//   //   });
+//   // }
+
+//   // const goToProductDetails = item => {
+//   //   console.log(item);
+//   //   navigation.navigate('ProductDetails', {productId: item});
+//   // };
+
+//   // const goToProductDetailsTablet = itemTablets => {
+//   //   console.log(itemTablets);
+//   //   navigation.navigate('ProductDetailsTablets', {tabletId: itemTablets});
+//   // };
+
+//   // const goToProductDetailsLaptop = itemLaptop => {
+//   //   console.log(itemLaptop);
+//   //   navigation.navigate('ProductDetailsLaptop', {laptopId: itemLaptop});
+//   // };
+
+//   // const goToProductDetailsMonitor = itemMonitor => {
+//   //   console.log(itemMonitor);
+//   //   navigation.navigate('ProductDetailsMonitor', {MonitorId: itemMonitor});
+//   // };
+
+//   const sendCred = (id) => {
+//     fetch('http://10.0.2.2:3000/ecommerce/', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         _id: id
+//       }),
+//     })
+//       .then(Response => Response.json())
+//       .then(data => {
+//         console.log(data);
+//       });
+//   };
+
+//   return (
+//     <ScrollView>
+//       <View>
+//         {id === 0 && (
+//           <View>
+//             {smartWatchesData.map(item => (
+//               <TouchableOpacity onPress={() => goToProductDetails(item.id)}>
+//                 <ProductListContain
+//                   key={item.id}
+//                   title={item.title}
+//                   price={item.price}
+//                   img={item.img}
+//                   rating={item.rating}
+//                 />
+//               </TouchableOpacity>
+//             ))}
+//           </View>
+//         )}
+
+//         {/* {id === 1 && (
+//           <View>
+//             {tabletsData.map(itemTablets => (
+//               <TouchableOpacity
+//                 onPress={() => goToProductDetailsTablet(itemTablets.id)}>
+//                 <ProductListContain
+//                   key={itemTablets.id}
+//                   title={itemTablets.title}
+//                   price={itemTablets.price}
+//                   img={itemTablets.img}
+//                   rating={itemTablets.rating}
+//                 />
+//               </TouchableOpacity>
+//             ))}
+//           </View>
+//         )}
+
+//         {id === 2 && (
+//           <View>
+//             {LaptopData.map(itemLaptop => (
+//               <TouchableOpacity
+//                 onPress={() => goToProductDetailsLaptop(itemLaptop.id)}>
+//                 <ProductListContain
+//                   key={itemLaptop.id}
+//                   title={itemLaptop.title}
+//                   price={itemLaptop.price}
+//                   img={itemLaptop.img}
+//                   rating={itemLaptop.rating}
+//                 />
+//               </TouchableOpacity>
+//             ))}
+//           </View>
+//         )}
+
+//         {id === 3 && (
+//           <View>
+//             {monitorData.map(itemMonitor => (
+//               <TouchableOpacity
+//                 onPress={() => goToProductDetailsMonitor(itemMonitor.id)}>
+//                 <ProductListContain
+//                   key={itemMonitor.id}
+//                   title={itemMonitor.title}
+//                   price={itemMonitor.price}
+//                   img={itemMonitor.img}
+//                   rating={itemMonitor.rating}
+//                 />
+//               </TouchableOpacity>
+//             ))}
+//           </View>
+//         )} */}
+//       </View>
+//     </ScrollView>
+//   );
+// };
+
+// export default ProductList;
+
+// This file use the Backend Node(Js) to map the data and display the Data using the Axios
+import React, {useState, useEffect} from 'react';
+import {View, TouchableOpacity, ScrollView, Text} from 'react-native';
 import ProductListContain from './productListContain';
-import {smartWatchesData} from './../data/smartWatchesData';
-import {ScrollView} from 'react-native-gesture-handler';
-import {tabletsData} from './../data/tabletsData';
-import {LaptopData} from './../data/laptopData';
-import {monitorData} from './../data/monitorData';
+import axios from 'axios';
 
 const ProductList = ({navigation, route}) => {
-  const id = route.params?.productId;
-  console.log('id:', id);
-  let selectedProduct;
-  if (id === 0) {
-    const selectedProduct = smartWatchesData.find(element => {
-      console.log(element);
-      return id === element.id;
-    });
-  } else if (id === 1) {
-    const selectedProduct = tabletsData.find(element => {
-      console.log(element);
-      return id === element.id;
-    });
-  } else if (id === 2) {
-    const selectedProduct = LaptopData.find(element => {
-      console.log(element);
-      return id === element.id;
-    });
-  }
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null); // add error state
+
+  useEffect(() => {
+    axios
+      .get('http://10.0.2.2:3000/ecommerce/productDetails')
+      .then(response => {
+        console.log(response.data); // check response data
+        if (Array.isArray(response.data.result)) {
+          // verify that response data is an array
+          setProducts(response.data.result);
+          setIsLoading(false);
+        } else {
+          setError('Response data is not an array.');
+          setIsLoading(false);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error.message); // set error state with error message
+        setIsLoading(false);
+      });
+  }, []);
 
   const goToProductDetails = item => {
-    console.log(item);
-    navigation.navigate('ProductDetails', {productId: item});
-  };
-
-  const goToProductDetailsTablet = itemTablets => {
-    console.log(itemTablets);
-    navigation.navigate('ProductDetailsTablets', {tabletId: itemTablets});
-  };
-
-  const goToProductDetailsLaptop = itemLaptop => {
-    console.log(itemLaptop);
-    navigation.navigate('ProductDetailsLaptop', {laptopId: itemLaptop});
-  };
-
-  const goToProductDetailsMonitor = itemMonitor => {
-    console.log(itemMonitor);
-    navigation.navigate('ProductDetailsMonitor', {MonitorId: itemMonitor});
+    navigation.navigate('ProductDetails', {productId: item._id});
   };
 
   return (
     <ScrollView>
-      <View>
-        {id === 0 && (
-          <View>
-            {smartWatchesData.map(item => (
-              <TouchableOpacity onPress={() => goToProductDetails(item.id)}>
-                <ProductListContain
-                  key={item.id}
-                  title={item.title}
-                  price={item.price}
-                  img={item.img}
-                  rating={item.rating}
-                />
-              </TouchableOpacity>
-            ))}
-          </View>
-        )}
-
-        {id === 1 && (
-          <View>
-            {tabletsData.map(itemTablets => (
-              <TouchableOpacity
-                onPress={() => goToProductDetailsTablet(itemTablets.id)}>
-                <ProductListContain
-                  key={itemTablets.id}
-                  title={itemTablets.title}
-                  price={itemTablets.price}
-                  img={itemTablets.img}
-                  rating={itemTablets.rating}
-                />
-              </TouchableOpacity>
-            ))}
-          </View>
-        )}
-
-        {id === 2 && (
-          <View>
-            {LaptopData.map(itemLaptop => (
-              <TouchableOpacity
-                onPress={() => goToProductDetailsLaptop(itemLaptop.id)}>
-                <ProductListContain
-                  key={itemLaptop.id}
-                  title={itemLaptop.title}
-                  price={itemLaptop.price}
-                  img={itemLaptop.img}
-                  rating={itemLaptop.rating}
-                />
-              </TouchableOpacity>
-            ))}
-          </View>
-        )}
-
-        {id === 3 && (
-          <View>
-            {monitorData.map(itemMonitor => (
-              <TouchableOpacity
-                onPress={() => goToProductDetailsMonitor(itemMonitor.id)}>
-                <ProductListContain
-                  key={itemMonitor.id}
-                  title={itemMonitor.title}
-                  price={itemMonitor.price}
-                  img={itemMonitor.img}
-                  rating={itemMonitor.rating}
-                />
-              </TouchableOpacity>
-            ))}
-          </View>
-        )}
-      </View>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : error ? (
+        <Text>{error}</Text> // show error message if error state is not null
+      ) : (
+        products.map(item => (
+          <TouchableOpacity
+            key={item._id}
+            onPress={() => goToProductDetails(item)}>
+            <ProductListContain
+              image={item.image}
+              title={item.title}
+              price={item.price}
+              rating={item.rating}
+            />
+          </TouchableOpacity>
+        ))
+      )}
     </ScrollView>
   );
 };
--- screens/productListContain.js
+++ screens/productListContain.js
@@ -1,8 +1,82 @@
+// This is the File which uses static data API to map the data
+
+// import {StyleSheet, Text, View, Image} from 'react-native';
+// import React from 'react';
+// import RatingStars from '../components/ratingStars';
+
+// const ProductListContain = ({img, title, price, rating, navigation}) => {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.topCont}>
+//         <View style={styles.imageCont}>
+//           <Image
+//             style={{
+//               height: '100%',
+//               width: '100%',
+//               //   borderColor: "#b8baba",
+//               //   borderWidth: 1,
+//               //   borderRadius: 10,
+//               resizeMode: 'contain',
+//               backgroundColor: 'white',
+//             }}
+//             source={{uri: img}}
+//           />
+//         </View>
+//         <View style={styles.contentCont}>
+//           <Text style={styles.itemTitleText}>{title}</Text>
+//           <RatingStars rating={rating} />
+//           <Text style={styles.priceText}>₹{price}</Text>
+//         </View>
+//       </View>
+//     </View>
+//   );
+// };
+
+// export default ProductListContain;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     borderBottomColor: 'white',
+//     borderBottomWidth: 10,
+//     backgroundColor: '#ededed',
+//     padding: 15,
+//   },
+//   topCont: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     padding: 10,
+//   },
+//   imageCont: {
+//     flex: 1,
+//   },
+//   contentCont: {
+//     flex: 2,
+//     padding: 5,
+//     marginLeft: 10,
+//   },
+//   itemTitleText: {
+//     flex: 1,
+//     fontWeight: '600',
+//     color: 'black',
+//     fontWeight: 'bold',
+//     marginBottom: 10,
+//   },
+//   priceText: {
+//     flex: 1,
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     marginBottom: 5,
+//     marginTop: 5,
+//     color: 'black',
+//   },
+// });
+
+// This is the File which uses the Node (Js) backend to map the data and uses the axios
 import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
 import RatingStars from '../components/ratingStars';
 
-const ProductListContain = ({img, title, price, rating, navigation}) => {
+const ProductListContain = ({image, title, price, rating}) => {
   return (
     <View style={styles.container}>
       <View style={styles.topCont}>
@@ -11,13 +85,10 @@ const ProductListContain = ({img, title, price, rating, navigation}) => {
             style={{
               height: '100%',
               width: '100%',
-              //   borderColor: "#b8baba",
-              //   borderWidth: 1,
-              //   borderRadius: 10,
               resizeMode: 'contain',
               backgroundColor: 'white',
             }}
-            source={{uri: img}}
+            source={{uri: image}}
           />
         </View>
         <View style={styles.contentCont}>
