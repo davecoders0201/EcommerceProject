19db110806daf1136327deff0817527a3bc99417
davecoders0201 <davecoder0201@gmail.com>
2023-04-05 13:53:16 +0530

    Remove unuse Things

--- screens/CheckoutForm.js
+++ screens/CheckoutForm.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import axios from 'axios';
 import {
   StripeProvider,
