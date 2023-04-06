733226a4c1b633f9fcbb35ffff18c9f430bc94d7
davecoders0201 <davecoder0201@gmail.com>
2023-04-06 11:04:36 +0530

    Cart Count increases when the Items added

--- navigation/tabNavigation.js
+++ navigation/tabNavigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {View, Text} from 'react-native';
 import CartScreen from '../screens/cartScreen';
@@ -6,9 +6,15 @@ import MenuScreen from '../screens/menuScreen';
 import HomeScreen from '../screens/homeScreen';
 import ProfileScreen from '../screens/profileScreen';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-
+import {useSelector} from 'react-redux';
 const TabNavigation = () => {
   const Tab = createBottomTabNavigator();
+  const items = useSelector(state => state);
+  const [cartCount, setCartCount] = useState(items.length);
+
+  useEffect(() => {
+    setCartCount(items.length);
+  }, [items]);
   return (
     <Tab.Navigator
       screenOptions={{
@@ -54,20 +60,22 @@ const TabNavigation = () => {
           tabBarIcon: ({color, size}) => (
             <View style={{position: 'relative'}}>
               <Icon name="cart-outline" color={color} size={size} />
-              <View
-                style={{
-                  position: 'absolute',
-                  top: -8,
-                  right: -12,
-                  backgroundColor: '#02c3d9',
-                  borderRadius: 10,
-                  minWidth: 20,
-                  height: 20,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <Text style={{color: '#fff', fontSize: 12}}>0</Text>
-              </View>
+              {cartCount >= 0 && (
+                <View
+                  style={{
+                    position: 'absolute',
+                    top: -8,
+                    right: -12,
+                    backgroundColor: '#02c3d9',
+                    borderRadius: 10,
+                    minWidth: 20,
+                    height: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text style={{color: '#fff', fontSize: 12}}>{cartCount}</Text>
+                </View>
+              )}
             </View>
           ),
         })}
